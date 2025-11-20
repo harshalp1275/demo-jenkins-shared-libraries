f1467e29dee31b667529ff226fbe9145d21a71f0
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-11-20 21:36:09 +0530

    Refactor build script to use parameters for Docker build

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,4 @@
-echo "This is building the code"
-                sh "whoami"
-                sh "docker build -t note-app:latest ."
+// Define function
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+}
