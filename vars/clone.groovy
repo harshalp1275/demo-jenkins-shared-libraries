023cfdd4a02d0519c4eae3f41ce6c8b9d48d8e10
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-11-20 20:24:02 +0530

    Add code cloning function in clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,6 @@
+def call(){
+
+echo "This is cloning the code"
+                git url: "https://github.com/harshalp1275/django-notes-app.git" , branch : "feature-harsh-test-django-app"
+                echo "code cloning successful"
+}
