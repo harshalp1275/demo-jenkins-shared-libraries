1cacffbb71212bdf7523bbac481005204d7ca5da
harshalp1275 <112923468+harshalp1275@users.noreply.github.com>
2025-11-20 21:39:42 +0530

    Refactor docker_push.groovy for improved credentials handling

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,6 @@
-echo "pushing the image to docker hub"
-                withCredentials([usernamePassword(credentialsId: 'dockerHubCred', 
-                                usernameVariable: 'dockerHubUser', 
-                                passwordVariable: 'dockerHubPass')]){
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag note-app:latest ${env.dockerHubUser}/note-app:latest"
-                sh "docker push ${env.dockerHubUser}/note-app:latest"
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+}
